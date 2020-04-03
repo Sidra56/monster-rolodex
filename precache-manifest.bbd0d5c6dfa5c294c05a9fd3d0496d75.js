055bc2fc615305e2671a479ad37ae2ab3da8f76d
sidra56 <sidraabdullah56@gmail.com>
2020-04-03 15:10:22 +0500

    Updates

---
+++ precache-manifest.bbd0d5c6dfa5c294c05a9fd3d0496d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27f4500978a60e22872290f181fe9bc",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3a026ac9360f5fa0eb87",
+    "url": "/monster-rolodex/static/css/main.b3a694a1.chunk.css"
+  },
+  {
+    "revision": "a4a9a92535ed4793cbee",
+    "url": "/monster-rolodex/static/js/2.9320a8c0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.9320a8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a026ac9360f5fa0eb87",
+    "url": "/monster-rolodex/static/js/main.a72b1373.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
